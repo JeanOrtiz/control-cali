04e1c0200489d0ed33b39e13e161153dbe335f1b
JeanOrtiz MacPro <macbook@MacBooks-MacBook-Pro.local>
2022-12-12 18:28:29 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { EditStudentComponent } from './edit-student/edit-student.component';
+import { ViewStudentComponent } from './view-student/view-student.component';
+const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full'},
+  { path: 'Home', component: HomeComponent },
+  { path: 'ViewStudent/:studentId', component: ViewStudentComponent },
+  { path: 'AddStudent', component: AddStudentComponent },
+  { path: 'EditStudent/:studentId', component: EditStudentComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
