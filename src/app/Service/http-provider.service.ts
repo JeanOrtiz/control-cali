a9cb1da9ee08161996b10681b9080b31770854b2
jeanortiz <jeanpaulortizescoto6@gmail.com>
2022-12-12 20:31:48 -0400

    created routes, applications, components and services

--- src/app/Service/http-provider.service.ts
+++ src/app/Service/http-provider.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WebApiService } from './web-api.service';
 
+var apiUrl = "http://localhost:8100/";
+
+var httpLink = {
+  getAllStudent: apiUrl + "/api/student/getAllStudent",
+  deleteStudentById: apiUrl + "/api/student/deleteStudent",
+  getStudentDetailById: apiUrl + "/api/student/getStudentDetailById",
+  saveStudent: apiUrl + "/api/student/saveStudent"
+}
 @Injectable({
   providedIn: 'root'
 })
 export class HttpProviderService {
+  constructor(private webApiService: WebApiService) { }
 
-  constructor() { }
-}
+  public getAllStudent(): Observable<any> {
+    return this.webApiService.get(httpLink.getAllStudent);
+  }
+  public deleteStudentById(model: any): Observable<any> {
+    return this.webApiService.post(httpLink.deleteStudentById + '?studentId=' + model, "");
+  }
+  public getStudentDetailById(model: any): Observable<any> {
+    return this.webApiService.get(httpLink.getStudentDetailById + '?studentId=' + model);
+  }
+  public saveStudent(model: any): Observable<any> {
+    return this.webApiService.post(httpLink.saveStudent, model);
+  }  
+}                          
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, Type } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { HttpProviderService } from '../Service/http-provider.service';
 
+@Component({
+  selector: 'ng-modal-confirm',
+  template: `
+  <div class="modal-header">
+    <h5 class="modal-title" id="modal-title">CONFIRMACION DE ELIMINACION</h5>
+    <button type="button" class="btn close" aria-label="Close button" aria-describedby="modal-title" (click)="modal.dismiss('Cross click')">
+      <span aria-hidden="true">×</span>
+    </button>
+  </div>
+  <div class="modal-body">
+    <p>Estas Seguro que quieres eliminarlo?</p>
+  </div>
+  <div class="modal-footer">
+    <button type="button" class="btn btn-outline-secondary" (click)="modal.dismiss('cancel click')">CANCELAR</button>
+    <button type="button" ngbAutofocus class="btn btn-success" (click)="modal.close('Ok click')">OK</button>
+  </div>
+  `,
+})
+export class NgModalConfirm {
+  constructor(public modal: NgbActiveModal) { }
+}
+const MODALS: { [name: string]: Type<any> } = {
+  deleteModal: NgModalConfirm,
+};
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  closeResult = '';
+  employeeList: any = [];
+student: any;
+  constructor(private router: Router, private modalService: NgbModal,
+    private toastr: ToastrService, private httpProvider : HttpProviderService) { }
 
+  ngOnInit(): void {
+    this.getAllStudent();
+  }
+  async getAllStudent() {
+    this.httpProvider.getAllStudent().subscribe((data : any) => {
+      if (data != null && data.body != null) {
+        var resultData = data.body;
+        if (resultData) {
+          this.employeeList = resultData;
+        }
+      }
+    },
+    (error : any)=> {
+        if (error) {
+          if (error.status == 404) {
+            if(error.error && error.error.message){
+              this.employeeList = [];
+            }
+          }
+        }
+      });
+  }
+
+  AddStudent() {
+    this.router.navigate(['AddStudent']);
+  }
+
+  deleteStudentConfirmation(employee: any) {
+    this.modalService.open(MODALS['deleteModal'],
+      {
+        ariaLabelledBy: 'modal-basic-title'
+      }).result.then((result) => {
+        this.deleteStudent(employee);
+      },
+        (reason) => {});
+  }
+
+  deleteStudent(student: any) {
+    this.httpProvider.deleteStudentById(student.id).subscribe((data : any) => {
+      if (data != null && data.body != null) {
+        var resultData = data.body;
+        if (resultData != null && resultData.isSuccess) {
+          this.toastr.success(resultData.message);
+          this.getAllStudent();
+        }
+      }
+    },
+    (error : any) => {});
+  }
+  getStudent() {
+    throw new Error('Method not implemented.');
+  }
 }
